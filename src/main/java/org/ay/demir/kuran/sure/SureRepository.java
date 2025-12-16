9281043a64932cc1c7be1293154a54a25faeb034
ayasindemir <ayasindemir@gmail.com>
2025-12-16 22:55:38 +0300

    acik_kuran

--- src/main/java/org/ay/demir/kuran/sure/SureRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ay.demir.kuran.sure;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SureRepository extends CrudRepository<Sure, Long> {
-
-}
